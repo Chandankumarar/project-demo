b8bf48701a7a7f8e4f57824df953ed4d5f3cdaed
Chandankumarar <chandan.89717@gmail.com>
2024-11-11 23:33:31 +0530

    added 2 java files

---
+++ javafiles/hello.java
@@ -0,0 +1,13 @@
+package javafiles;
+
+public class hello {
+    public static void main(String[] args) {
+        //to print even numbers till 30
+        int i;
+        for(i=1;i<=30;i++)
+        {
+            if(i%1==0 && i%i==1)
+                System.out.println(i);
+        }
+    }
+}
